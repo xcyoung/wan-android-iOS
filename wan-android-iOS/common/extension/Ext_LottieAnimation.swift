5af58067fd48bf65b287e89afeb7343b8799f8af
肖楚🐑 <xiaochu@xiaochudeMacBook-Pro.local>
2020-08-14 00:16:02 +0800

    feat: 暂时去除Lottie对于夜间模式的适配

--- wan-android-iOS/common/extension/Ext_LottieAnimation.swift
+++ wan-android-iOS/common/extension/Ext_LottieAnimation.swift
@@ -12,16 +12,16 @@ extension Animation {
     static func nameWithMode(_ name: String, subdirectory: String? = nil) -> Animation? {
         let realName: String
         let realSubdirectory: String
-        if #available(iOS 13.0, *) {
-            let currentMode = UITraitCollection.current.userInterfaceStyle
-            if (currentMode == .dark) {
-                realName = name + "_dark"
-            } else {
-                realName = name
-            }
-        } else {
+//        if #available(iOS 13.0, *) {
+//            let currentMode = UITraitCollection.current.userInterfaceStyle
+//            if (currentMode == .dark) {
+//                realName = name + "_dark"
+//            } else {
+//                realName = name
+//            }
+//        } else {
             realName = name
-        }
+//        }
         realSubdirectory = "animation/\(name)"
         return named(realName, subdirectory: realSubdirectory)
     }
